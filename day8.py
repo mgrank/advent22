75157bd2797c4abe3d49a14e522a6af15a47854c
Michael Grankin <theflint@gmail.com>
2022-12-11 15:56:12 +0300

    day8 part1 deduplicated

--- day8.py
+++ day8.py
@@ -4,39 +4,22 @@ with open('day8test.txt') as f:
     tree_line = [int(tree) for tree in list(line.strip())]
     trees.append(tree_line)
 
+visibility = [[0 for _ in line] for line in trees] #table that shows if particular tree is visible (1) or not (0)
 
-visibility = [[0 for _ in line] for line in trees]
-visible = 0
+look_directions = [ (range(len(trees[0])), range(len(trees))), #forward and down
+                    (range(len(trees[0])-1,-1,-1), range(len(trees)-1,-1,-1)) ] #backward and up
 
-x_index = [range(len(trees[0])), range(len(trees[0])-1,-1,-1)] #forward and backward
-
-max_height = [-1] * len(trees)
-for x in range(len(trees[0])):
-  for y in range(len(trees)):
-    if trees[y][x] > max_height[y]:
-      max_height[y] = trees[y][x]
-      visibility[y][x] = 1
-
-max_height = [-1] * len(trees)
-for x in range(len(trees[0])-1,-1,-1):
-  for y in range(len(trees)):
-    if trees[y][x] > max_height[y]:
-      max_height[y] = trees[y][x]
-      visibility[y][x] = 1
-
-max_height = [-1] * len(trees[0])
-for x in range(len(trees[0])):
-  for y in range(len(trees)):
-    if trees[y][x] > max_height[x]:
-      max_height[x] = trees[y][x]
-      visibility[y][x] = 1
-
-max_height = [-1] * len(trees[0])
-for x in range(len(trees[0])):
-  for y in range(len(trees)-1,-1,-1):
-    if trees[y][x] > max_height[x]:
-      max_height[x] = trees[y][x]
-      visibility[y][x] = 1
+for x_range, y_range in look_directions:
+  max_height_y = [-1] * len(trees)
+  max_height_x = [-1] * len(trees[0])
+  for x in x_range:
+    for y in y_range:
+      if trees[y][x] > max_height_y[y]:
+        max_height_y[y] = trees[y][x]
+        visibility[y][x] = 1
+      if trees[y][x] > max_height_x[x]:
+        max_height_x[x] = trees[y][x]
+        visibility[y][x] = 1
 
 visible = sum([sum(line) for line in visibility])
 print(visible)
